df6a0b4e1cefdddeb75c94a28bc57735d50beb2f
Jason Long <jasonl@opendental.od>
2021-07-12 07:31:44 -0700

    ready

--- my-application/src/app/app.module.ts
+++ my-application/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MyLibModule } from 'my-lib';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MyLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
